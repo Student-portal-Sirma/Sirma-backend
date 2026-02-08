915149eaf2484c57c74723eb0ebbdaa75b041614
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2026-02-08 10:07:53 +0200

    Edit Content Types

--- src/api/course/controllers/courses-teachers.ts
+++ src/api/course/controllers/courses-teachers.ts
@@ -1,23 +1,23 @@
 export default {
-    async find(ctx: any) {
-        const courses = await strapi.documents("api::course.course").findMany({
-            fields: ["Title", "Description", "Technology", "Level"],
-            populate: {
-                enrollments: {
-                    filters: {
-                        role: {
-                            $eq: "teacher",
-                        },
-                    },
-                    populate: {
-                        user: {
-                            fields: ["username"],
-                        },
-                    },
-                },
+  async find(ctx: any) {
+    const courses = await strapi.documents("api::course.course").findMany({
+      fields: ["Title", "Description", "Level"],
+      populate: {
+        enrollments: {
+          filters: {
+            role: {
+              $eq: "teacher",
             },
-        });
+          },
+          populate: {
+            user: {
+              fields: ["username"],
+            },
+          },
+        },
+      },
+    });
 
-        ctx.body = courses;
-    },
+    ctx.body = courses;
+  },
 };
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,18 +1,18 @@
-import type { Schema, Struct } from '@strapi/strapi';
+import type { Schema, Struct } from "@strapi/strapi";
 
 export interface DocumentsDocs extends Struct.ComponentSchema {
-  collectionName: 'components_documents_docs';
+  collectionName: "components_documents_docs";
   info: {
-    displayName: 'Docs';
-    icon: 'archive';
+    displayName: "Docs";
+    icon: "archive";
   };
   attributes: {};
 }
 
-declare module '@strapi/strapi' {
+declare module "@strapi/strapi" {
   export module Public {
     export interface ComponentSchemas {
-      'documents.docs': DocumentsDocs;
+      "documents.docs": DocumentsDocs;
     }
   }
 }
