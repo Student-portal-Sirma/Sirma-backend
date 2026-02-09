85ef4e640f2636bb7258d685c69b3bce9e91850c
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2026-02-09 18:05:46 +0200

    Add new endpoint for modules

--- src/api/course/controllers/course-users.ts
+++ src/api/course/controllers/course-users.ts
@@ -1,21 +1,21 @@
 export default {
-  async extended(ctx: any) {
-    const { documentId } = ctx.params;
+    async extended(ctx: any) {
+        const { documentId } = ctx.params;
 
-    const courses = await strapi.documents("api::course.course").findOne({
-      documentId,
-      fields: ["Title", "Description", "Level"],
-      populate: {
-        enrollments: {
-          populate: {
-            user: {
-              fields: ["username"],
+        const courses = await strapi.documents("api::course.course").findOne({
+            documentId,
+            fields: ["Title", "Description", "Level"],
+            populate: {
+                enrollments: {
+                    populate: {
+                        user: {
+                            fields: ["username"],
+                        },
+                    },
+                },
             },
-          },
-        },
-      },
-    });
+        });
 
-    ctx.body = courses;
-  },
+        ctx.body = courses;
+    },
 };
